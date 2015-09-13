185b7bc851b141f1f4ae0a1410b802793b99d6bb
Parthe Pandit <parthe1292@gmail.com>
2015-09-13 09:48:09 +0530

    Changing the get_mfcc funcion from another local repo on the same machine.

--- get_mfcc.m
+++ get_mfcc.m
@@ -1,3 +1,3 @@
 function MFCC = get_mfcc (sig,fs,win_len,win_overlap,win_shape)
-
+% This function is the feature extraction step in the speaker diarization task. For the Speech-Activity-Detection and Speaker-Clustering derivatives of MFCCs are used, whereas for the Speaker-Segmentation only the MFCC coefficients with the Short time energy are used.
 end
